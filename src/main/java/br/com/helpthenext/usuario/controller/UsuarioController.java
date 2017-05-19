f28072216a2dea35c5cb6b5ffbd8a01edee2149a
Ana <Ana@192.168.0.3>
2017-05-19 00:26:19 -0300

    Tela inicial mais tela de login

--- src/main/java/br/com/helpthenext/usuario/controller/UsuarioController.java
+++ src/main/java/br/com/helpthenext/usuario/controller/UsuarioController.java
@@ -36,22 +36,17 @@ public class UsuarioController implements Serializable {
 	}
  
 	public UsuarioModel GetUsuarioSession(){
- 
 		FacesContext facesContext = FacesContext.getCurrentInstance();
- 
 		return (UsuarioModel)facesContext.getExternalContext().getSessionMap().get("usuarioAutenticado");
 	}
  
 	public String Logout(){
- 
 		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
- 
 		return "/index.xhtml?faces-redirect=true";
 	}
+	
 	public String EfetuarLogin(){
- 
 		if(StringUtils.isEmpty(usuarioModel.getUsuario()) || StringUtils.isBlank(usuarioModel.getUsuario())){
- 
 			Uteis.Mensagem("Favor informar o login!");
 			return null;
 		}
@@ -61,30 +56,22 @@ public class UsuarioController implements Serializable {
 			return null;
 		}
 		else{	
- 
 			usuarioEntity = usuarioRepository.ValidaUsuario(usuarioModel);
  
 			if(usuarioEntity!= null){
- 
 				usuarioModel.setSenha(null);
 				usuarioModel.setCodigo(usuarioEntity.getCodigo());
  
- 
 				FacesContext facesContext = FacesContext.getCurrentInstance();
- 
 				facesContext.getExternalContext().getSessionMap().put("usuarioAutenticado", usuarioModel);
- 
  
 				return "sistema/home?faces-redirect=true";
 			}
 			else{
- 
 				Uteis.Mensagem("Não foi possível efetuar o login com esse usuário e senha!");
 				return null;
 			}
 		}
- 
- 
 	}
  
 }
